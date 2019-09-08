45d735563e48b8d222d24f47862430f6c367a6e7
A Groshek <amy@amygroshek.com>
2019-09-08 12:18:28 -0500

    Updates data files and rendering of viz

--- themes/base-hugo-theme/static/js/viz/viz.js
+++ themes/base-hugo-theme/static/js/viz/viz.js
@@ -1,102 +1,283 @@
 console.log('viz.js loaded');
-console.log(viz.theme);
-console.log(viz.nodes);
-console.log(viz.nodes[0]);
+// console.log(viz.theme);
+// console.log(viz.parents);
+// console.log(viz.children);
+// console.log(viz.parents[0]);
+// console.log(viz.links);
 const setup = {};
-setup.categories = [];
 setup.nodes = [];
 setup.links = [];
 let linkIndex = 0;
 let colorIndex = 0;
-const item = {
+const symbolSizeBase = 20;
+// Track who is hovered
+viz.active = {};
+viz.active.hovered = null;
+viz.active.clicked = null;
+// Tooltip for topics and subtopics
+const topicTooltip = {
+  // trigger: 'item',
+  formatter: '{c}' + '<br>Click for more',
+  show: true,
+  position: 'right',
+  // itemStyle: {
+    // color: '#000',
+    backgroundColor: 'transparent',
+    textStyle: {
+      color: '#000',
+      fontWeight: 'bold',
+      fontFace: 'sofia-pro'
+    }
+  // }
+}
+const subtopicTooltip = {
+
+}
+// Label for topics and subtopics
+const topicLabel = {
+  // position: 'right',
+  // show: false,
+  normal: {
+    position: 'right',
+    show: function(el) {
+      console.log('show' + (el.data.id == viz.active.hovered));
+      return el.data.id == viz.active.hovered;
+      // return false;
+    },
+    textStyle: {
+      color: '#000',
+      fontWeight: 'bold',
+      fontFace: 'sofia-pro',
+      fontSize: 16
+    },
+  },
+  // itemStyle: {
+  //   // color: 'black',
+  //   textStyle: {
+  //     color: '#000',
+  //     fontWeight: 'bold',
+  //     fontFace: 'sofia-pro',
+  //     fontSize: 20
+  //   }
+  // },
+  // textStyle: {
+  //   color: '#000',
+  //   fontWeight: 'bold',
+  //   fontFace: 'sofia-pro',
+  //   fontSize: 20
+  // },
+  formatter: function(el) {
+    console.log('formatter', el);
+    if (el.data.id === viz.active.hovered) {
+      return el.data.value + '<br>Click for more';
+    } else {
+      return el.data.value + 'blah';
+    }
+  },
+  // color: '#000',
+}
+const subtopicLabel = {
+  show: false,
+}
+// Add root node
+const root = {
+  id: 'root',
   name: "ubi",
   value: "UBI",
   category: 'root',
-  fixed: false,
+  // fixed: true,
   symbol: 'circle',
-  symbolSize: 30,
+  symbolSize: 80,
   itemStyle: {
     color: viz.theme[colorIndex]
-  }
+  },
+  tooltip: {
+    show: false
+  },
+  label: {
+    show: true,
+    position: 'inside',
+    formatter: '{c}',
+    textStyle: {
+      // color: '#000',
+      fontSize: 20,
+      fontWeight: 'normal',
+      fontFace: 'sofia-pro'
+    }
+  },
 };
-(setup.nodes).push(item);
+(setup.nodes).push(root);
 colorIndex++;
-viz.nodes.forEach(function(el) {
+
+viz.parents.forEach(function(el) {
   // console.log(i);
   // console.log(el.title);
-  // Add item to nodes array
-  const item = {
-    name: el.id,
-    value: el.title,
-    category: el.id,
-    fixed: false,
-    symbol: 'circle',
-    symbolSize: 20,
-    itemStyle: {
-      color: viz.theme[colorIndex]
-    }
-  };
-  (setup.nodes).push(item);
-  // Add children to nodes array
-  el.children.forEach((el) => {
+  if (!!el.display) {
+    // Add item to nodes array
     const item = {
-      name: el.id,
+      id: el.id,
+      name: el.title,
       value: el.title,
       category: el.id,
-      fixed: false,
+      // fixed: true,
       symbol: 'circle',
-      symbolSize: 10,
+      symbolSize: 40,
       itemStyle: {
         color: viz.theme[colorIndex]
-      }
+      },
+      tooltip: topicTooltip,
+      label: topicLabel
     };
     (setup.nodes).push(item);
-  });
-  // If node category not already in categories, add it.
-  if (!setup.categories[name === el.id]) {
-    (setup.categories).push({
-      name: el.id,
-      itemStyle: {
-        color: viz.theme[colorIndex]
-      }
-    });
+    colorIndex++;
   }
-  // Build links obj
-  // Link to parent
+});
+console.log('Done adding parent nodes');
+console.log(setup.nodes);
+
+viz.children.forEach(function(el) {
+  // console.log(i);
+  // console.log(el.title);
+  const _parent = setup.nodes.filter(
+    function(value){ return value.id === el.parent }
+  )
+  const _viz_parent = viz.parents.filter(
+    function(value){ return value.id === el.parent }
+  )
+  // console.log('_parent');
+  // console.log(_parent);
+  if (!!el.display && !!_viz_parent[0].display) {
+    // console.log('display is on');
+    // Add item to nodes array
+    const item = {
+      id: el.id,
+      name: el.title,
+      value: el.title,
+      category: el.id,
+      // fixed: true,
+      symbol: 'circle',
+      symbolSize: 20,
+      itemStyle: {
+        color: _parent[0].itemStyle.color,
+      },
+      tooltip: topicTooltip,
+      label: topicLabel
+    };
+    (setup.nodes).push(item);
+  }
+});
+console.log('Done adding child nodes');
+console.log(setup.nodes);
+
+viz.links.forEach(function(el) {
   const link = {
     id: linkIndex,
-    source: 'root',
-    target: el.id
+    source: el.source,
+    target: el.target
   };
   (setup.links).push(link);
   linkIndex++;
-  // Links to siblings
-  (el.linksTo).forEach((link) => {
-    const linkObj = viz.nodes.find(o => o.id === link);
-    const linkTo = {
-      id: linkIndex,
-      source: el.id,
-      target: linkObj.id
-    };
-    (setup.links).push(linkTo);
-    linkIndex++;
-  });
-  // Links to children
-  (el.children).forEach((child) => {
-    const childLink = {
-      id: linkIndex,
-      source: el.id,
-      target: child.id
-    };
-    (setup.links).push(childLink);
-    linkIndex++;
-  });
-  // Increment color index for next "theme".
-  colorIndex++;
 });
-console.log('setup.categories');
-console.log(setup.categories);
+
+
+// console.log('setup.categories');
+// console.log(setup.categories);
 console.log('setup.nodes');
 console.log(setup.nodes);
 console.log('setup.links');
 console.log(setup.links);
+setup.options = {
+  title: {
+    // text: 'Les Miserables',
+    subtext: 'Default layout',
+    top: 'bottom',
+    left: 'right'
+  },
+  // toolbox: {
+  //   feature: {
+  //     dataZoom: {
+  //       show: true,
+  //       title: {
+  //         zoom: 'Zoom',
+  //         back: 'Back'
+  //       }
+  //     }
+  //   }
+  // },
+  series : [
+    {
+      name: 'UBI',
+      type: 'graph',
+      layout: 'force',
+      force: {
+        repulsion: 95,
+        gravity: 0.015,
+        edgeLength: 40,
+        layoutAnimation: false
+      },
+      data: setup.nodes,
+      links: setup.links,
+      // categories: setup.categories,
+      focusNodeAdjacency: true,
+      itemStyle: {
+        normal: {
+          borderColor: '#fff',
+          borderWidth: 1,
+          shadowBlur: 10,
+          shadowColor: 'rgba(0, 0, 0, 0.3)'
+        }
+      },
+      lineStyle: {
+        color: 'source',
+        curveness: 0.3
+      },
+      emphasis: {
+        lineStyle: {
+          width: 10
+        }
+      },
+      roam: true,
+    }
+  ]
+};
+/**
+ * Stops scrolling over the eCharts div
+ * @param  Object e Event object
+ * @return null
+ */
+function stopWheel(e){
+  if(!e){ /* IE7, IE8, Chrome, Safari */
+    e = window.event;
+  }
+  if(e.preventDefault) { /* Chrome, Safari, Firefox */
+    e.preventDefault();
+  }
+  e.returnValue = false; /* IE7, IE8 */
+}
+jQuery('#viz-space').hover(function() {
+  jQuery(document).bind('mousewheel DOMMouseScroll',function(){
+    stopWheel();
+  });
+}, function() {
+  jQuery(document).unbind('mousewheel DOMMouseScroll');
+});
+// Init eCharts
+viz.chart = echarts.init(document.getElementById('viz-space'));
+viz.chart.showLoading();
+viz.chart.setOption(setup.options);
+viz.chart.hideLoading();
+viz.chart.on('click', (e) => {
+  // console.log(e);
+  viz.active.clicked = e.data.id;
+});
+viz.chart.on('mouseover', (e) => {
+  console.log('mouseover');
+  console.log(e);
+  viz.active.hovered = e.data.id;
+  console.log(viz.active);
+});
+viz.chart.on('mouseout', (e) => {
+  // console.log(e);
+  viz.active.hovered = null;
+  console.log(viz.active);
+})
