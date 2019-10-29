91f507c63920029f9542b28b74637ee0aae36459
James Minton <james-minton@github.com>
2019-10-29 19:12:05 -0400

    Implement JS hover drop-down, remove some vestigial code

--- themes/base-hugo-theme/static/js/bil-custom.js
+++ themes/base-hugo-theme/static/js/bil-custom.js
@@ -30,6 +30,38 @@
         }
     }
 
+    /** Dropdown on hover */
+    $(".dropdown-toggle").hover( function () {
+      // Open up the dropdown
+      $(this).removeAttr('data-toggle'); // remove the data-toggle attribute so we can click and follow link
+      $(this).parent().addClass('show'); // add the class show to the li parent
+      $(this).next().addClass('show'); // add the class show to the dropdown div sibling
+    }, function () {
+      // on mouseout check to see if hovering over the dropdown or the link still
+      var isDropdownHovered = $(this).next().filter(":hover").length; // check the dropdown for hover - returns true of false
+      var isThisHovered = $(this).filter(":hover").length;  // check the top level item for hover
+      if(isDropdownHovered || isThisHovered) {
+          // still hovering over the link or the dropdown
+      } else {
+          // no longer hovering over either - lets remove the 'show' classes
+          $(this).attr('data-toggle', 'dropdown'); // put back the data-toggle attr
+          $(this).parent().removeClass('show');
+          $(this).next().removeClass('show');
+      }
+    });
+    // Check the dropdown on hover
+    $(".dropdown-menu").hover( function () {
+    }, function() {
+      var isDropdownHovered = $(this).prev().filter(":hover").length; // check the dropdown for hover - returns true of false
+      var isThisHovered= $(this).filter(":hover").length;  // check the top level item for hover
+      if(isDropdownHovered || isThisHovered) {
+          // do nothing - hovering over the dropdown of the top level link
+      } else {
+          // get rid of the classes showing it
+          $(this).parent().removeClass('show');
+          $(this).removeClass('show');
+      }
+    });
 
     var updateModal = {
         activeBio: null,
@@ -175,32 +207,6 @@
         $(this).addClass("highlight");
     });
 
-    if ($('body.home').length >= 1) {
-      console.log('setting up home page animations');
-      setElPositions();
-        $(window).resize(function() {
-        setElPositions();
-      })
-      setElPositions();
-
-      var userScrolled = false;
-      var svgScrollEvt = $(window).scroll(function() {
-        userScrolled = true;
-      });
-      var svgScrollInt = setInterval(function() {
-        if (avgGraphicAnimated && growthGraphicAnimated && trendGraphicAnimated) {
-          // Remove listener and interval
-          // console.log('removing listener and interval');
-          $(window).off("scroll", svgScrollEvt);
-          clearInterval(svgScrollInt);
-        } else {
-          if (userScrolled) {
-            setupAnime();
-            checkHomepageAnimations();
-            userScrolled = false;
-          }
-        }
-      }, 50);
-    }
+  
   });
 })(jQuery);
