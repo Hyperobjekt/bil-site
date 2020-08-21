d19c414ae8984ff1b67607fae22b5afcf7a706b0
Josh Maas-Howard <j.maas.howard@gmail.com>
2020-08-21 12:14:42 -0400

    toolbox zoom controls in

--- themes/base-hugo-theme/static/js/viz/viz.js
+++ themes/base-hugo-theme/static/js/viz/viz.js
@@ -6,22 +6,13 @@
 // _ X implement tabbed subthemes
 // _ export
 // 
-// 
-// 
 
 //   viz.chart.dispatchAction({
 //     type: 'downplay/highlight', 
 //     name: 'Work',
 //    })
-// viz.chart.dispatchAction({
-//     type: 'focusNodeAdjacency',
-// Use seriesId or seriesIndex or seriesName to specify
-// the target series.
-// Use either `dataIndex` or `edgeDataIndex` to specify
-// the target node or target edge.
-//     dataIndex: 14,
-// })
 
+const ZOOM_CONST = 2;
 
 console.log('viz.js loaded');
 // console.log(viz.theme);
@@ -243,6 +234,67 @@ viz.rebuild();
 // console.log('Done adding child nodes');
 // console.log(setup.nodes);
 setup.options = {
+  toolbox: {
+    show: true,
+    bottom: 20,
+    left: 20,
+    orient: 'vertical',
+    // showTitle: false,
+    // itemGap: 10,
+    title: {
+      position: 'right'
+    },
+    zlevel: 40000,
+    feature: {
+      myZoomIn: {
+        show: true,
+        title: ' ',
+        icon: 'M13.388,9.624h-3.011v-3.01c0-0.208-0.168-0.377-0.376-0.377S9.624,6.405,9.624,6.613v3.01H6.613c-0.208,0-0.376,0.168-0.376,0.376s0.168,0.376,0.376,0.376h3.011v3.01c0,0.208,0.168,0.378,0.376,0.378s0.376-0.17,0.376-0.378v-3.01h3.011c0.207,0,0.377-0.168,0.377-0.376S13.595,9.624,13.388,9.624z M10,1.344c-4.781,0-8.656,3.875-8.656,8.656c0,4.781,3.875,8.656,8.656,8.656c4.781,0,8.656-3.875,8.656-8.656C18.656,5.219,14.781,1.344,10,1.344z M10,17.903c-4.365,0-7.904-3.538-7.904-7.903S5.635,2.096,10,2.096S17.903,5.635,17.903,10S14.365,17.903,10,17.903z',
+        onclick: function () {
+          const options = viz.chart.getOption();
+          viz.zoomLevel = options.series[0].zoom *= ZOOM_CONST;
+          
+          viz.chart.setOption(options);
+          // TODO: reset focus, 
+          // viz.chart.dispatchAction({
+          //   type: 'focusNodeAdjacency',
+          //   dataIndex: viz.active.clicked
+          // })
+        }
+      },
+      myZoomOut: {
+        show: true,
+        title: ' ',
+        icon: 'M10,1.344c-4.781,0-8.656,3.875-8.656,8.656c0,4.781,3.875,8.656,8.656,8.656c4.781,0,8.656-3.875,8.656-8.656C18.656,5.219,14.781,1.344,10,1.344z M10,17.903c-4.365,0-7.904-3.538-7.904-7.903S5.635,2.096,10,2.096S17.903,5.635,17.903,10S14.365,17.903,10,17.903z M13.388,9.624H6.613c-0.208,0-0.376,0.168-0.376,0.376s0.168,0.376,0.376,0.376h6.775c0.207,0,0.377-0.168,0.377-0.376S13.595,9.624,13.388,9.624z',
+        onclick: function () {
+          const options = viz.chart.getOption();
+          viz.zoomLevel = options.series[0].zoom /= ZOOM_CONST;
+          
+          viz.chart.setOption(options);
+          // TODO: reset focus, 
+          // viz.chart.dispatchAction({
+          //   type: 'focusNodeAdjacency',
+          //   dataIndex: viz.active.clicked
+          // })
+        }
+      },
+      myRestore: {
+        show: true,
+        showTitle: true,
+        title: 'Restore',
+        icon: 'M3.254,6.572c0.008,0.072,0.048,0.123,0.082,0.187c0.036,0.07,0.06,0.137,0.12,0.187C3.47,6.957,3.47,6.978,3.484,6.988c0.048,0.034,0.108,0.018,0.162,0.035c0.057,0.019,0.1,0.066,0.164,0.066c0.004,0,0.01,0,0.015,0l2.934-0.074c0.317-0.007,0.568-0.271,0.56-0.589C7.311,6.113,7.055,5.865,6.744,5.865c-0.005,0-0.01,0-0.015,0L5.074,5.907c2.146-2.118,5.604-2.634,7.971-1.007c2.775,1.912,3.48,5.726,1.57,8.501c-1.912,2.781-5.729,3.486-8.507,1.572c-0.259-0.18-0.618-0.119-0.799,0.146c-0.18,0.262-0.114,0.621,0.148,0.801c1.254,0.863,2.687,1.279,4.106,1.279c2.313,0,4.591-1.1,6.001-3.146c2.268-3.297,1.432-7.829-1.867-10.101c-2.781-1.913-6.816-1.36-9.351,1.058L4.309,3.567C4.303,3.252,4.036,3.069,3.72,3.007C3.402,3.015,3.151,3.279,3.16,3.597l0.075,2.932C3.234,6.547,3.251,6.556,3.254,6.572z',
+        onclick: function () {
+          viz.chart.dispatchAction({
+            type: 'restore',
+          });
+
+          jQuery(`#text-panel .collapse[data-id='${viz.active.clicked}'`).collapse('hide');
+          viz.active.clicked = null;
+
+        }
+      },
+    },
+  },
   title: {
     show: false,
     text: 'Exploring Universal Basic Income',
@@ -261,7 +313,7 @@ setup.options = {
   // textStyle: {
   //   extraCssText: 'z-index:500;'
   // },
-  series : [
+  series: [
     {
       name: 'UBI',
       type: 'graph',
@@ -383,8 +435,6 @@ viz.chart.on('mouseover', function(e) {
     viz.active.hovered = e.data.id;
     // viz.rebuild();
     // viz.chart.setOption(setup.options);
-
-
   }
 })
     
@@ -443,7 +493,7 @@ viz.chart.on('click', function(e) {
     }, 100);
   }
 });
-viz.zoomlevel = 0;
+viz.zoomLevel = 0;
 // Zoom event listener
 viz.chart.on('graphRoam', function(e) {
   console.log('zoomed');
@@ -456,7 +506,7 @@ viz.chart.on('graphRoam', function(e) {
     } else {
       viz.zoomLevel = viz.zoomLevel + Math.abs( e.zoom - 1 );
     }
-    // console.log('viz.zoomLevel = ' + viz.zoomLevel);
+    console.log('viz.zoomLevel = ' + viz.zoomLevel);
     // Update label display based on zoom level
     if (viz.zoomLevel > 0.75) {
       subtopicLabel.normal.show = true;
